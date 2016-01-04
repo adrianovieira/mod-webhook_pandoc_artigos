83f96a07bf4445ed3dee8604a2cb0f51857eca34
Adriano Vieira <adriano.vieira@dataprev.gov.br>
2016-01-04 16:34:36 -0200

    Reestrutura parâmetros em classe específica (params)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,5 @@
 class webhook_pandoc_artigos (
-        $webhook_service_name = 'webhook-dev.puppet',
-        $webhook_service_name_aliases = 'webhook-dev',
-        $webhook_docroot = '/var/www/webhook',
-        $webhook_script_aliases = '/artigos-2pdf',
-        $webhook_pdfdownload_aliases = '/artigos-download',
-        $webhook_pdfdownload_aliases_path = '/var/tmp/webhook_tmp',
-        $webhook_markdowntemplate_path = '/var/share/markdown-template/',
-        $webhook_gitlab_user_name = hiera('webhook_gitlab_user_name','admin'),
-        $webhook_gitlab_user_pass = hiera('webhook_gitlab_user_pass','secret'),
-        $dtp_puppetversion_min = '3.6.2',
-        $dtp_puppetversion_max = '3.8.4',
-        $exec_environment = undef, # environment for exec
-        $webhook_wsgi_hello = false,  # initial setup test (hello, world)
-        $webhook_wsgi_hello_flask = false, # initial setup test (hello, world by flask)
-        )
+        ) inherits webhook_pandoc_artigos::params
 {
   /*
     # verifica puppet suportado
@@ -44,7 +30,7 @@ class webhook_pandoc_artigos (
     }
   }
 
-  info("Operating system ${::osfamily}-${::operatingsystemrelease} supported")
+  info("Operating system ${::osfamily}-${::operatingsystemrelease} supported (env: [${environment}])")
 
   # http: WEBHOOK
   include apache
