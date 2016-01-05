d2d0d663381854dc4bcafdff367344df96c035f4
Adriano Vieira <adriano.vieira@dataprev.gov.br>
2016-01-05 12:18:19 -0200

    Corrige valor padrão para puppetversion

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,8 +8,8 @@ class webhook_pandoc_artigos::params (
         $webhook_markdowntemplate_path = hiera('webhook_markdowntemplate_path'), # '/var/share/markdown-template/'
         $webhook_gitlab_user_name = hiera('webhook_gitlab_user_name'), # 'admin'
         $webhook_gitlab_user_pass = hiera('webhook_gitlab_user_pass'), # 'secret'
-        $dtp_puppetversion_min = hiera('dtp_puppetversion_min'), # '3.6.2'
-        $dtp_puppetversion_max = hiera('dtp_puppetversion_max'), # '3.8.4'
+        $dtp_puppetversion_min = hiera('dtp_puppetversion_min', '3.6.2'),
+        $dtp_puppetversion_max = hiera('dtp_puppetversion_max', '3.8.4'),
         $exec_environment = undef, # environment for exec
         $webhook_wsgi_hello = hiera('webhook_wsgi_hello',false),  # initial setup test (hello, world)
         $webhook_wsgi_hello_flask = hiera('webhook_wsgi_hello_flask',false), # initial setup test (hello, world by flask)
